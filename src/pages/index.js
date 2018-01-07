e7e493a1a45ec5bcbc7d4f9c6c087f2b9dfa2aec
Jonas <jonaso@web.de>
2018-01-07 13:43:38 +0100

    update index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,15 +8,34 @@ import img from "../img/opp.jpg"
 
 const travel = {
   upcoming: [
-    { event: "HCOMP'18, 5.7.-8.7.2018, Zürich", status: "planned" },
-    { event: "MKWI'18, 6.3.-9.3.2018, Lüneburg", status: "confirmed" },
-    { event: "GI-Symposium, 29.1.2018, Berlin", status: "confirmed" },
+    { event: "HCOMP'18, 5.-8. July 2018, Zürich", status: "planned" },
+    { event: "Future of Work and Innovation Symposium, 15. May 2018, Berlin", status: "planned" },
+    { event: "MKWI'18, 6.-9. March 2018, Lüneburg", status: "confirmed" },
+    { event: "GI-Symposium, 29. January 2018, Berlin", status: "confirmed" },
   ],
   past: [
     { event: "HCOMP'17, 24.10.-26.10.2017, Quebec", status: "confirmed" },
   ],
 }
 
+const keywords = {
+  primary: [
+    "collaborative ontology engineering",
+    "semantic web",
+    "linked data",
+    "crowdsourcing",
+    "human computation",
+    "collective intelligence",
+  ],
+  secondary: [
+    "social machines",
+    "knowledge transfer",
+    "computer supported collaborative work",
+    "e-science",
+    "hypertext and hypermedia",
+    "online communities",
+  ],
+}
 
 const styles = {
     nobottommargin: {
@@ -58,126 +77,130 @@ const styles = {
 
 
 class Home extends React.Component {
-    render() {
-        return (
-            <div>
-              <Container style={{marginBottom: '14px'}}>
+  render() {
+    return (
+      <div>
+        <Container style={{marginBottom: '14px'}}>
 
-                <Grid>
-                  <Grid.Row>
+          <Grid>
+            <Grid.Row>
 
-                    <Grid.Column mobile={8} tablet={8} computer={8} widescreen={8} largeScreen={8}>
+              <Grid.Column mobile={8} tablet={8} computer={8} widescreen={8} largeScreen={8}>
 
-                      <p>
-                        I am <strong>Jonas Oppenlaender</strong>,
-                        an Early-Stage Researcher
-                        at the <a href="http://www.mi.fu-berlin.de/en/inf/groups/hcc/">Human-Centered Computing</a> group at FU Berlin
-                        working towards a PhD in Computer Science or Business Informatics (Information Science).
-                      </p>
+                <p>
+                  I am <strong>Jonas Oppenlaender</strong>,
+                  an Early-Stage Researcher
+                  at the <a href="http://www.mi.fu-berlin.de/en/inf/groups/hcc/">Human-Centered Computing</a> group at FU Berlin
+                  working towards a PhD in Computer Science.
+                </p>
 
-                      <p>
-                        I studied Industrial Engineering in Germany and Sweden
-                        and hold a MSc degree in Computer Science
-                        from the <a href="https://www.southampton.ac.uk/">University of Southampton</a>, UK.
-                        <br />
-                        Presently, I am in the process of identifying topics and finding a supervisor for my PhD thesis.
-                        Two possible topics that particularly interest me are the application of crowdsourcing in order to solve problems in the context of the semantic web
-                        and tackling the broader problem of providing better working conditions and fair compensation to crowdworkers.
-                      </p>
+                <p>
+                  I studied Industrial Engineering in Germany and Sweden
+                  and hold a MSc degree in Computer Science
+                  from the <a href="https://www.southampton.ac.uk/">University of Southampton</a>, UK.
+                </p>
 
-                      <Header size="tiny" style={{...styles.nobottommargin, ...styles.notopmargin}}>Office Address:</Header>
-                      <p>
-                          Human-Centered Computing Group<br />
-                          Room 115<br />
-                          Institute of Computer Science, Freie Universit&auml;t Berlin<br />
-                          K&ouml;nigin-Luise-Str. 24-26, 14195 Berlin, Germany<br />
-                          E-Mail: {'{'}firstname.lastname{'}'}@fu-berlin.de<br />
-                          Telephone: +49 30 838-61565<br />
-                          Fax: +49 30 838-475233<br />
-                      </p>
+                <p>
+                  I am interested in building web-based socio-technical systems
+                  in which the human and technological elements play the role of participant machinery
+                  in order to perform complex tasks better than either human or machine could independently.
+                  <br />
+                  This exploratory, open-ended approach to research is conducted in the context of
+                  Business Informatics (BI), a research discipline that originated in Germany.
+                  Unlike Information Science, BI tends towards constructing and evaluating prototypical artifacts (Design Science Research).
+                </p>
 
-                      <p>
-                          <strong>Consultation hour</strong>: <i>on demand</i> - just send me an email and we&apos;ll arrange a meeting
-                      </p>
+                <p>
+                  Presently, I am in the process of identifying topics and finding a supervisor for my PhD thesis.
+                  Two possible topics that particularly interest me are the application of crowdsourcing in order to solve problems in the context of the semantic web
+                  and tackling the broader problem of providing better working conditions and fair compensation to crowdworkers.
+                </p>
 
-                      <p>
-                          <Icon as="i" link={true} size="large" name='hand outline right' />
-                          <a href="http://www.mi.fu-berlin.de/en/inf/groups/hcc/theses/open/index.html">Open BSc and MSc theses</a>
-                      </p>
+                <Header size="tiny" style={{...styles.nobold, ...styles.notopmargin, ...styles.nobottommargin}}>My <strong>primary research interests</strong> are:</Header>
+                  { keywords.primary.join(", ") }
 
-                      <strong>Upcoming Travel:</strong>
-                      <List>
-                        {
-                          travel.upcoming.map(item => (
-                            <List.Item>
-                                {item.status === 'confirmed' ?
-                                  <List.Icon name='checkmark' title="confirmed" /> :
-                                  <List.Icon name='calendar' title="planned" />
-                                }
-                                {item.event}
-                            </List.Item>
-                          ))
-                        }
-                      </List>
+                <Header size="tiny" style={{...styles.nobold, ...styles.notopmargin, ...styles.nobottommargin}}><strong>Further research interests</strong> include:</Header>
+                  { keywords.secondary.join(", ") }
 
-                </Grid.Column>
 
-                <Grid.Column mobile={8} tablet={8} computer={6}>
+                <Header size="tiny" style={styles.nobottommargin}>Upcoming Travel</Header>
+                <List>
+                  {
+                    travel.upcoming.map(item => (
+                      <List.Item>
+                          {item.status === 'confirmed' ?
+                            <List.Icon name='checkmark' title="confirmed" /> :
+                            <List.Icon name='calendar' title="planned" />
+                          }
+                          {item.event}
+                      </List.Item>
+                    ))
+                  }
+                </List>
 
-                  <Responsive minWidth={768}>
-                    <Image src={img} size='medium' rounded />
-                  </Responsive>
+              </Grid.Column>
 
-                  <Responsive maxWidth={769}>
-                    <Image src={img} rounded fluid />
-                  </Responsive>
+              <Grid.Column mobile={8} tablet={8} computer={6}>
 
-                  <Header size="tiny" style={{...styles.nobold, ...styles.nobottommargin}}>My <strong>primary research interests</strong> are:</Header>
-                    collaborative ontology engineering,
-                    semantic web,
-                    linked data,
-                    crowdsourcing,
-                    human computation,
-                    collective intelligence
+                <Responsive minWidth={768}>
+                  <Image src={img} size='medium' rounded />
+                </Responsive>
 
-                  <Header size="tiny" style={{...styles.nobold, ...styles.nobottommargin}}><strong>Further research interests</strong> include:</Header>
-                    social machines,
-                    knowledge transfer,
-                    computer supported collaborative work,
-                    e-science,
-                    hypertext and hypermedia,
-                    online communities
+                <Responsive maxWidth={769}>
+                  <Image src={img} rounded fluid />
+                </Responsive>
 
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
 
-              <Divider />
+                <Header size="tiny" style={styles.nobottommargin}>Office Address:</Header>
+                <p>
+                    Human-Centered Computing Group<br />
+                    Room 115<br />
+                    Institute of Computer Science, Freie Universit&auml;t Berlin<br />
+                    K&ouml;nigin-Luise-Str. 24-26, 14195 Berlin, Germany<br />
+                    E-Mail: {'{'}firstname.lastname{'}'}@fu-berlin.de<br />
+                    Telephone: +49 30 838-61565<br />
+                    Fax: +49 30 838-475233<br />
+                </p>
 
-              <a href="https://www.linkedin.com/in/jonasopp/" target="_blank">
-                  <Icon title="LinkedIn" link={true} color="grey" size="big" name='linkedin square' />
-              </a>
+                <p>
+                    <strong>Consultation hour</strong>: <i>on demand</i> - just send me an email and we&apos;ll arrange a meeting
+                </p>
 
-              <a href="https://github.com/joetm" target="_blank">
-                  <Icon title="GitHub" link={true} color="grey" size="big" name='github square' />
-              </a>
+                <p>
+                    <Icon as="i" link={true} size="large" name='hand outline right' />
+                    <a href="http://www.mi.fu-berlin.de/en/inf/groups/hcc/theses/open/index.html">Open BSc and MSc theses</a>
+                </p>
 
-              <a href="https://twitter.com/Duesynapse" target="_blank">
-                  <Icon title="Twitter" link={true} color="grey" size="big" name='twitter square' />
-              </a>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
 
-              <a href="https://scholar.google.com/citations?hl=de&user=ucO_QYQAAAAJ" target="_blank">
-                  <i style={styles.academicon} title="Google Scholar" className="ai ai-google-scholar-square ai-2x"></i>
-              </a>
+          <Divider />
 
-              <a href="https://www.researchgate.net/profile/Jonas_Oppenlaender" target="_blank">
-                  <i style={styles.academicon} title="Researchgate" className="ai ai-researchgate-square ai-2x"></i>
-              </a>
+          <a href="https://www.linkedin.com/in/jonasopp/" target="_blank">
+              <Icon title="LinkedIn" link={true} color="grey" size="big" name='linkedin square' />
+          </a>
 
-              </Container>
-            </div>
-        )
-    }
+          <a href="https://github.com/joetm" target="_blank">
+              <Icon title="GitHub" link={true} color="grey" size="big" name='github square' />
+          </a>
+
+          <a href="https://twitter.com/Duesynapse" target="_blank">
+              <Icon title="Twitter" link={true} color="grey" size="big" name='twitter square' />
+          </a>
+
+          <a href="https://scholar.google.com/citations?hl=de&user=ucO_QYQAAAAJ" target="_blank">
+              <i style={styles.academicon} title="Google Scholar" className="ai ai-google-scholar-square ai-2x"></i>
+          </a>
+
+          <a href="https://www.researchgate.net/profile/Jonas_Oppenlaender" target="_blank">
+              <i style={styles.academicon} title="Researchgate" className="ai ai-researchgate-square ai-2x"></i>
+          </a>
+
+        </Container>
+      </div>
+    )
+  }
 }
 
 export default Home
