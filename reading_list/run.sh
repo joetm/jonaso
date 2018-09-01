bb0d2637019d6277e97d7dcc1c01625dfd5d156c
Jonas <jonaso@web.de>
2018-09-01 14:58:00 +0300

    update reading list

---
+++ reading_list/run.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+START_TIME=$SECONDS
+
+./start-docker.sh &
+
+./parsefiles.py
+
+docker-stop-all
+
+./update.sh
+
+ELAPSED_TIME=$(($SECONDS - $START_TIME))
+
+echo "Finished in $(($ELAPSED_TIME/60)) min $(($ELAPSED_TIME%60)) sec"
+
