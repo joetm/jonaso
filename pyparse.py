625e0205820e78338237540b35c3be5830c77a61
Jonas <jonaso@web.de>
2019-05-08 19:35:07 +0100

    parse pubs per type

--- pyparse.py
+++ pyparse.py
@@ -4,12 +4,27 @@
 
 import json
 import bibtexparser
+from bibtexparser.bparser import BibTexParser
+from bibtexparser.bibdatabase import as_text
+
+parser = BibTexParser()
+parser.ignore_nonstandard_types = False
+parser.homogenise_fields = True
+parser.common_strings = False
+
 
 with open('src/bibliography/publications.bib') as bibtex_file:
-    bib_database = bibtexparser.load(bibtex_file)
+    bib_database = bibtexparser.load(bibtex_file, parser)
 
-# print(bib_database.entries)
+print(bib_database.entries)
+# print(bib_database.strings)
+
+# print(as_text(bib_database.entries[0]['title']))
+
+# bibtex_str = bibtexparser.dumps(bib_database)
+# print(bibtex_str)
+
 
 # write as json
-with open('./references.json', 'w') as outfile:  
+with open('./references-detail.json', 'w') as outfile:
     json.dump(bib_database.entries, outfile, indent=4, sort_keys=True)
--- src/pages/publications.js
+++ src/pages/publications.js
@@ -5,8 +5,20 @@ import "isomorphic-fetch"
 
 import { spacer } from "../common"
 
+const styles = {
+  menu: {
+    textAlign: 'center',
+    fontSize: '1.5em',
+    linHeight: '2.5em',
+  },
+  // menuLink: {
+  //   textDecoration: 'underline',
+  //   cursor: 'pointer',
+  // },
+}
 
-const _REFERENCES = "http://jonaso.de/static/references.json"
+const _REFERENCES_PER_YEAR = "http://jonaso.de/static/references.json"
+const _REFERENCES_PER_TYPE = "http://jonaso.de/static/references-detail.json"
 
 
 /**
@@ -14,7 +26,7 @@ const _REFERENCES = "http://jonaso.de/static/references.json"
  * @param {Array} pubList - List of publications
  * @returns {Object} categorizedList - Categorized list of publications
  */
-function categorizeList(pubList) {
+function categorizeListPerYear(pubList) {
     if (!pubList) {
         return {}
     }
@@ -34,28 +46,119 @@ function categorizeList(pubList) {
     return categorizedList
 }
 
+/**
+ * Group publications by type
+ * @param {Array} pubList - List of publications
+ * @returns {Object} categorizedList - Categorized list of publications
+ */
+// function categorizeListPerType(pubList) {
+//     if (!pubList) {
+//         return {}
+//     }
+//     const categorizedList = {}
+//     pubList.forEach(obj => {
+//       if (categorizedList[obj.howpublished]) {
+//           categorizedList[obj.howpublished].push(obj)
+//       } else {
+//           categorizedList[obj.howpublished] = [obj]
+//       }
+//     })
+//     return categorizedList
+// }
+
 
 class Publications extends React.Component {
   state = {
     references: {},
+    referencesDetail: {},
   }
   componentWillMount = () => {
-    fetch(_REFERENCES)
+    // fetch references per publication year
+    fetch(_REFERENCES_PER_YEAR)
     .then(response => response.json())
     .then(refs => {
-      const categorizedRefs = categorizeList(refs)
+      const categorizedRefs = categorizeListPerYear(refs)
       this.setState({references: categorizedRefs})
     })
+    // fetch references per publication type
+    // fetch(_REFERENCES_PER_TYPE)
+    // .then(response => response.json())
+    // .then(referencesDetail => {
+    //   // const categorizedRefs = categorizeListPerType(refs)
+    //   this.setState({referencesDetail})
+    // })
   }
+  // switchPubView () {
+  //   alert('switch');
+  // }
   render() {
     const { references } = this.state
-    const keys = Object.keys(references).reverse()
+    const keysYear = Object.keys(references).reverse()
+    // const keysType = Object.keys(referencesDetail).reverse()
+    // keysType.sort()
     return (
       <div>
-        <Container id="publications">
+
+{/*
+        <div style={styles.menu}>
+          <button onClick={this.switchPubView} title="Publications per year">YEAR</button>
+          {" "} | {" "}
+          <button onClick={this.switchPubView} title="Publications per type">TYPE</button>
+        </div>
+
+        <Container id="publications-type" style={{display:'block'}}>
 
               {
-                keys.map(year => {
+                keysType.map(typ => {
+                  return (
+                    <Grid key={typ}>
+                        <Grid.Row>
+                          <Header size="large">{typ}</Header>
+                        </Grid.Row>
+                        {
+                          referencesDetail[typ].map((item, index) => {
+                            item.__html = item.__html.replace('Jonas Oppenlaender', '<strong>Jonas Oppenlaender</strong>')
+                            item.__html = item.__html.replace('Jonas Oppenländer', '<strong>Jonas Oppenländer</strong>')
+                            let icostr = 'file text outline'
+                            if (item.__html.indexOf('.pdf') === -1) {
+                              icostr = 'file outline'
+                            }
+                            return (
+                              <Grid.Row key={index}>
+                                <Grid.Column width={2}>
+                                  <Responsive maxWidth={768}>
+                                    <Icon color="grey" size="large" name={icostr} />
+                                  </Responsive>
+                                  <Responsive minWidth={769} maxWidth={990}>
+                                    <Icon color="grey" size="huge" name={icostr} />
+                                  </Responsive>
+                                  <Responsive minWidth={992}>
+                                    <Icon color="grey" size="big" name={icostr} />
+                                  </Responsive>
+                                </Grid.Column>
+                                <Grid.Column width={14}>
+                                <Item>
+                                  <Item.Content>
+                                    <Item.Header dangerouslySetInnerHTML={item.__html} />
+                                  </Item.Content>
+                                </Item>
+                                </Grid.Column>
+                              </Grid.Row>
+                            )
+                          })
+                        }
+                    </Grid>
+                  )
+                })
+              }
+
+        </Container>
+*/}
+
+        <Container id="publications-year">
+
+              {
+                keysYear.map(year => {
                   return (
                     <Grid key={year}>
                         <Grid.Row>
@@ -104,7 +207,6 @@ class Publications extends React.Component {
                   )
                 })
               }
-
 
             <div style={spacer}></div>
 
