f5d6b47499ae8b37cf1c284d8f3304b4a83bb0df
Jonas <jonaso@web.de>
2019-10-11 20:33:36 +0300

    update influencers

--- src/pages/keywords.js
+++ src/pages/keywords.js
@@ -5,9 +5,33 @@ import { ResponsiveContainer, Treemap, BarChart, Bar, Tooltip, XAxis, YAxis, Car
 
 
 class Keywords extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isZoomed: false,
+      activeTooltipIndex: false,
+      activeLabel: null,
+      subcontent: [],
+    }
+  }
+  handleBackButtonClick = () => {
+    this.setState({isZoomed: false})
+  }
+  handleClick = (e) => {
+    let {isZoomed} = this.state
+    isZoomed = !isZoomed
+    if (isZoomed) {
+      // DEV
+      let subcontent = [{name:'A',num:12},{name:'B',num:12}]
+      this.setState({subcontent})
+    }
+    this.setState({isZoomed})
+    console.log(e, 'zoomed:', isZoomed);
+  }
   render() {
     const { keywords = [] } = this.props
-    // console.log('keywords', keywords);
+    const { isZoomed, subcontent } = this.state
+    console.log('keywords', keywords);
 
     // filtered_keywords = keywords.map(kw => kw.num > 1 ? kw : null);
     // console.log('filtered_keywords', filtered_keywords);
@@ -15,27 +39,38 @@ class Keywords extends React.Component {
     return (
         <Container>
           <h2>Research Interests</h2>
+
           <ResponsiveContainer width="100%" height={960}>
-            <BarChart layout="vertical" data={keywords}>
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis
-                type="number"
-                dataKey="num"
-              />
-              <YAxis
-                type="category"
-                dataKey="name"
-                width={200}
-              />
-              <Tooltip
-                separator=" "
-                formatter={(value, name, props) => (<span>Publications: {value}</span>)}
-              />
-              <Bar
-                dataKey="num"
-                fill="#82ca9d"
-              />
-            </BarChart>
+                <BarChart layout="vertical" data={keywords}>{/* onClick={this.handleClick} */}
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis
+                    type="number"
+                    dataKey="num"
+                  />
+                  <YAxis
+                    type="category"
+                    dataKey="name"
+                    width={200}
+                  />
+                  <Tooltip
+                    separator=" "
+                    formatter={(value, name, props) => (<span>Publications: {value}</span>)}
+                  />
+                  <Bar
+                    dataKey="num"
+                    fill="#82ca9d"
+                  />
+                </BarChart>
+{/*
+                :
+                <Treemap
+                  data={subcontent}
+                  isAnimationActive={true}
+                  animationDuration={1000}
+                  dataKey="num"
+                  onClick={this.handleClick}
+                />
+*/}
           </ResponsiveContainer>
 
         </Container>
