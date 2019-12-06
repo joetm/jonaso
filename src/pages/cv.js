f0635e14fa8a0c1faff1707b216c092367cc2cec
Jonas <jonaso@web.de>
2019-12-06 14:10:52 +0200

    update

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -118,7 +118,7 @@ class CV extends React.Component {
                         <Item.Description style={styles.nomarginTop}>
                           <List>
                             <List.Item><a href="http://www.oulu.fi/university/">University of Oulu</a>, Oulu, Finland</List.Item>
-                            <List.Item>Ph.D. Candidate</List.Item>
+                            <List.Item>Doctoral Student</List.Item>
                             <List.Item>Supervisor: <a href="http://simohosio.com/">Adjunct Prof. Dr. Simo Hosio</a></List.Item>
                           </List>
                         </Item.Description>
