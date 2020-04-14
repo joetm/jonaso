f4e49576d46be581bcce03845dec907946bdff3a
Jonas <jonaso@web.de>
2020-04-14 13:03:08 +0300

    update

--- reading_list/start-docker.sh
+++ reading_list/start-docker.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 # docker run -m=4g -p 8080:8080 --rm allenai/scienceparse:2.0.1
-docker run -p 8080:8080 --rm allenai/scienceparse:2.0.1
+# docker run -p 8080:8080 --rm allenai/scienceparse:2.0.1
+docker run -p 8080:8080 --rm allenai/scienceparse:2.0.3
