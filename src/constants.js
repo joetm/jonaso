4fc011df8a0ee215aabe92f6111a4e08114e3d0b
Jonas <jonaso@web.de>
2020-10-16 17:59:50 +0300

    update influencers

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+
+const prod = {
+  _REFERENCES_PER_YEAR: "https://raw.githubusercontent.com/joetm/jonaso/master/public/static/references.json",
+  _REFERENCES_PER_TYPE: "https://raw.githubusercontent.com/joetm/jonaso/master/public/static/references-type.json",
+}
+
+const dev = {
+  _REFERENCES_PER_YEAR: "http://localhost:8000/static/references.json",
+  _REFERENCES_PER_TYPE: "http://localhost:8000/static/references-type.json",
+}
+
+export const config = process.env.NODE_ENV === ‘development’ ? dev : prod
