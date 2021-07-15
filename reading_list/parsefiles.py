d6dbd8a70369a0be0f3143eeae251eb29df4c9b2
Jonas <jonaso@web.de>
2021-07-15 19:26:31 +0200

    update cv

--- reading_list/parsefiles.py
+++ reading_list/parsefiles.py
@@ -5,6 +5,7 @@
 import json
 import sqlite3
 import time
+import sys
 
 from libs import fileReader
 
@@ -19,12 +20,22 @@ BASEPATHS = [
     "/media/jonas/Research/FU/IKON/Literatur/",
     "/media/jonas/Research/FU/Academic Working/",
     "/media/jonas/Research/FU/ID+LAB/Literatur/",
-    "/media/jonas/Research/FU/IKON/Ontologies",
+    # "/media/jonas/Research/FU/IKON/Ontologies",
     "/media/jonas/Research/FU/Misc/"
 ]
 
 PATTERN_READ = "[\!|\-]*.pdf"
 
+
+
+# define Python user-defined exceptions
+class Error(Exception):
+    """Base class for other exceptions"""
+    pass
+
+class FilesNotFound(Error):
+    """Raised when no files were found"""
+    pass
 
 
 # def main():
@@ -34,48 +45,58 @@ print("Mining pdfs")
 conn = sqlite3.connect('cache.db')
 
 
-documents, counter, unrecognizedCounter = fileReader.parsefiles(PATTERN_READ, BASEPATHS, conn)
+try:
+    documents, counter, unrecognizedCounter = fileReader.parsefiles(PATTERN_READ, BASEPATHS, conn)
+    if len(documents) == 0:
+        raise FilesNotFound
 
 
-# write the full list
-if counter == 0:
-    unrecognizedCounterPercent = 0
-else:
-    unrecognizedCounterPercent = unrecognizedCounter / counter
-out = {
-        'modified': int(time.time()),
-        'unrecognized': unrecognizedCounter,
-        'unrecognized_percent': unrecognizedCounterPercent,
-        'documents': documents
-    }
-with open('readlist-full.json', 'w') as LISTFILE:
-    json.dump(out, LISTFILE, indent=4)
+    # write the full list
+    if counter == 0:
+        unrecognizedCounterPercent = 0
+    else:
+        unrecognizedCounterPercent = unrecognizedCounter / counter
+    out = {
+            'modified': int(time.time()),
+            'unrecognized': unrecognizedCounter,
+            'unrecognized_percent': unrecognizedCounterPercent,
+            'documents': documents
+        }
+    with open('readlist-full.json', 'w') as LISTFILE:
+        json.dump(out, LISTFILE, indent=4)
 
 
-# latest-100
-del documents[100:]
+    # latest-100
+    del documents[100:]
 
-# count unrecognized in latest-100
-unrecognizedD = 0
-for d in documents:
-    if not d['title']:
-        unrecognizedD = unrecognizedD + 1
+    # count unrecognized in latest-100
+    unrecognizedD = 0
+    for d in documents:
+        if not d['title']:
+            unrecognizedD = unrecognizedD + 1
 
-# write the latest-100 list
-out = {
-       'modified': int(time.time()),
-        'unrecognized_overall': unrecognizedCounter,
-        'unrecognized_overall_percent': round(unrecognizedCounterPercent, 2),
-        'unrecognized': unrecognizedD,
-        'unrecognized_percent': round(unrecognizedD / len(documents), 2),
-        'documents': documents
-    }
-with open('readlist-latest.json', 'w') as LISTFILE:
-    json.dump(out, LISTFILE, indent=4)
+    # write the latest-100 list
+    out = {
+           'modified': int(time.time()),
+            'unrecognized_overall': unrecognizedCounter,
+            'unrecognized_overall_percent': round(unrecognizedCounterPercent, 2),
+            'unrecognized': unrecognizedD,
+            'unrecognized_percent': round(unrecognizedD / len(documents), 2),
+            'documents': documents
+        }
+    with open('readlist-latest.json', 'w') as LISTFILE:
+        json.dump(out, LISTFILE, indent=4)
+
+
+except:
+    print("Files not found. USB connected?")
+    conn.close()
+    sys.exit(1)
 
 
 # close sqlite connection
 conn.close()
+
 
 # if __name__ == "__main__":
 #     main()
