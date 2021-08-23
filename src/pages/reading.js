de6ceb1624bad3c05cb2e5ebcfe599af055940e7
Jonas <jonaso@web.de>
2021-08-23 11:41:35 +0200

    fix line break on reading page

--- src/pages/reading.js
+++ src/pages/reading.js
@@ -80,7 +80,7 @@ class ReadingList extends React.Component {
               <div style={styles.unrecognized}>Unrecognized overall: <span>{unrecognized_overall} ({unrecognized_overall_percent * 100}%)</span></div>
             </Header>
 
-            <Table padded style={{'table-layout':'fixed'}} collapsing={false}>
+            <Table padded collapsing={false}>
               <Table.Header>
                 <Table.Row>
                   <Table.HeaderCell>Title</Table.HeaderCell>
@@ -96,8 +96,8 @@ class ReadingList extends React.Component {
                   documents.map((doc, idx) => {
                     return (
                       <Table.Row key={`id_${idx}`}>
-                        <Table.Cell textAlign="left">{doc.title}</Table.Cell>
-                        <Table.Cell textAlign="left">{doc.authors.join(', ')}</Table.Cell>
+                        <Table.Cell style={{'word-break':'break-all'}} textAlign="left">{doc.title}</Table.Cell>
+                        <Table.Cell style={{'word-break':'break-all'}} textAlign="left">{doc.authors.join(', ')}</Table.Cell>
                         <Table.Cell textAlign="left">{doc.year}</Table.Cell>
                         <Table.Cell textAlign="left">{doc.keywords}</Table.Cell>
                         <Table.Cell textAlign="center">{doc.priority}</Table.Cell>
