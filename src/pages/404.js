f1e8920148c86d67270186de3de1c5c50784475c
Jonas <jonaso@web.de>
2021-09-24 11:50:50 +0100

    update

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,12 @@
 import React from "react"
 import { Container } from 'semantic-ui-react'
 import Layout from "../components/layout"
-import { Helmet } from "react-helmet"
 
 
-class Home extends React.Component {
+class Page404 extends React.Component {
     render() {
         return (
             <Layout>
-                <Helmet>
-                  <meta charSet="utf-8" />
-                  <title>404 - Not Found {'//'} jonaso.de</title>
-                </Helmet>
                 <Container>
                   404 - Not Found.
                 </Container>
@@ -20,4 +15,4 @@ class Home extends React.Component {
     }
 }
 
-export default Home
+export default Page404
