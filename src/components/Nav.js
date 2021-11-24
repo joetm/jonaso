5f9b77bd2613a4a68672ff39cf61601e702c9096
Jonas <jonaso@web.de>
2021-11-24 12:42:15 +0100

    better menu on smaller screen sizes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,15 @@
 import React from 'react'
-import { Menu } from 'semantic-ui-react'
+import { Menu, Icon } from 'semantic-ui-react'
 import { navigate } from "gatsby"
+
+import { createMedia } from "@artsy/fresnel"
+const { MediaContextProvider, Media } = createMedia({
+  // breakpoints values can be either strings or integers
+  breakpoints: {
+    sm: 0,
+    md: 768,
+  },
+})
 
 const styles = {
   navSpacer: {
@@ -11,7 +20,8 @@ const styles = {
 
 export default class Nav extends React.Component {
   state = {
-    activeItem: 'home'
+    activeItem: 'home',
+    menuIsOpen: false,
   }
   createMenu = ({item}) => {
     const { activeItem } = this.state
@@ -31,21 +41,46 @@ export default class Nav extends React.Component {
       navigate(`/${name}`)
     }
   }
+  toggleMenu = (e) => {
+    e.preventDefault()
+    this.setState({menuIsOpen: !this.state.menuIsOpen})
+  }
   componentDidMount() {
     this.setState({activeItem: window.location.pathname.replace(/\//g, '') || 'home'})
   }
   render() {
     const MenuItem = this.createMenu
+    const menuIsOpen = this.state.menuIsOpen
+    console.log('menuIsOpen', menuIsOpen)
     return (
       <header>
-        <Menu fluid pointing stackable secondary widths="6" style={styles.navSpacer}>
-          <MenuItem item='Home' />
-          <MenuItem item='Publications' />
-          <MenuItem item='Projects' />
-          <MenuItem item='CV' />
-          <MenuItem item='Reading' />
-          <MenuItem item='Interests' />
-        </Menu>
+        <MediaContextProvider>
+          <Media at="sm">
+            <Menu fluid pointing stackable secondary widths="6" style={styles.navSpacer}>
+              <Menu.Item style={{cursor:'pointer'}} onClick={this.toggleMenu}>
+                <Icon name="sidebar" size="large" />
+              </Menu.Item>
+              <div style={{display: menuIsOpen ? 'block' : 'none'}}>
+                <MenuItem item='Home' />
+                <MenuItem item='Publications' />
+                <MenuItem item='Projects' />
+                <MenuItem item='CV' />
+                <MenuItem item='Reading' />
+                <MenuItem item='Interests' />
+              </div>
+            </Menu>
+          </Media>
+          <Media at="md">
+            <Menu fluid pointing stackable secondary widths="6" style={styles.navSpacer}>
+              <MenuItem item='Home' />
+              <MenuItem item='Publications' />
+              <MenuItem item='Projects' />
+              <MenuItem item='CV' />
+              <MenuItem item='Reading' />
+              <MenuItem item='Interests' />
+            </Menu>
+          </Media>
+        </MediaContextProvider>
       </header>
     )
   }
