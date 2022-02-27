89d1bad9426f3e84c7ca17d0b5b39ec1f05bb5a9
Jonas <jonaso@web.de>
2022-02-27 18:09:06 +0200

    reading table visibility on mobile

--- src/pages/reading.js
+++ src/pages/reading.js
@@ -82,19 +82,19 @@ class ReadingList extends React.Component {
 
             <Header size="large">
               100 Recently Read Publications
-              <div style={styles.lastupdate}>Last updated: <span>{modified}</span></div>
+              <div style={styles.lastupdate}><span className="mobilehide">Last updated:</span> <span>{modified}</span></div>
               <div style={styles.unrecognized}>Unrecognized overall: <span>{unrecognized_overall} ({unrecognized_overall_percent * 100}%)</span></div>
             </Header>
 
-            <Table padded collapsing={false}>
+            <Table padded collapsing={false} stackable striped size='small'>
               <Table.Header>
                 <Table.Row>
                   <Table.HeaderCell>Title</Table.HeaderCell>
-                  <Table.HeaderCell style={{maxWidth:'100px'}}>Author(s)</Table.HeaderCell>
-                  <Table.HeaderCell collapsing>Year</Table.HeaderCell>
+                  <Table.HeaderCell style={{maxWidth:'450px'}}>Author(s)</Table.HeaderCell>
+                  <Table.HeaderCell className="mobilehide" collapsing>Year</Table.HeaderCell>
                   <Table.HeaderCell collapsing>Keywords</Table.HeaderCell>
-                  <Table.HeaderCell title="Relevance to my past or current research OR importance to the respective field">Relevance/<br />Importance</Table.HeaderCell>
-                  <Table.HeaderCell>Read</Table.HeaderCell>
+                  <Table.HeaderCell className="mobilehide" title="Relevance to my past or current research OR importance to the respective field">Relevance/<br />Importance</Table.HeaderCell>
+                  <Table.HeaderCell className="mobilehide">Read</Table.HeaderCell>
                 </Table.Row>
               </Table.Header>
               <Table.Body>
@@ -103,11 +103,11 @@ class ReadingList extends React.Component {
                     return (
                       <Table.Row key={`id_${idx}`}>
                         <Table.Cell style={{wordBreak:'break-all'}} textAlign="left">{doc.title}</Table.Cell>
-                        <Table.Cell style={{wordBreak:'break-all',maxWidth:'100px'}} textAlign="left">{doc.authors.join(', ')}</Table.Cell>
-                        <Table.Cell textAlign="left">{doc.year}</Table.Cell>
+                        <Table.Cell style={{wordBreak:'break-all',maxWidth:'450px'}} textAlign="left">{doc.authors.join(', ')}</Table.Cell>
+                        <Table.Cell className="mobilehide" textAlign="left">{doc.year}</Table.Cell>
                         <Table.Cell textAlign="left">{doc.keywords}</Table.Cell>
-                        <Table.Cell textAlign="center">{doc.priority}</Table.Cell>
-                        <Table.Cell textAlign="left">{this.getDate(doc.modified * 1000)}</Table.Cell>
+                        <Table.Cell className="mobilehide" textAlign="center">{doc.priority}</Table.Cell>
+                        <Table.Cell className="mobilehide" textAlign="left">{this.getDate(doc.modified * 1000)}</Table.Cell>
                       </Table.Row>
                     )
                   })
