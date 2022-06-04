236caa74623a1aeb5495484e5cc44125727a3a5e
Jonas <jonaso@web.de>
2022-06-04 09:37:18 +0300

    file queries for artwork page

---
+++ src/pages/art-TWITTER.js
@@ -0,0 +1,252 @@
+// import 'semantic-ui-css/components/card.min.css'
+
+// import '../masonry.css'
+
+import React from "react"
+// Card, Image, Label
+import { Container, Header } from 'semantic-ui-react'
+import Layout from "../components/layout"
+import { Helmet } from "react-helmet"
+import { spacer } from "../common"
+// import SimpleReactLightbox from 'simple-react-lightbox'
+// import { SRLWrapper } from "simple-react-lightbox"
+
+// responsiveness
+// import { createMedia } from "@artsy/fresnel"
+// const { MediaContextProvider, Media } = createMedia({
+//   breakpoints: {
+//     sm: 0,
+//     md: 768,
+//     lg: 990,
+//   },
+// })
+
+
+// const _ARTWORKS = "/static/artworks/artworks.json"
+
+const styles = {
+	breadcrumb: {
+		cursor: 'pointer',
+	}
+}
+
+// function Cardwork(w) {
+// 	const work = w.info
+// 	const numtiles = w.numtiles
+// 	const labelsize = numtiles >= 3 ? {fontSize:".6em"} : {}
+// 	let methods = []
+// 	if (work.method instanceof Array) {
+// 		methods = work.method
+// 	} else {
+// 		methods.push(work.method)
+// 	}
+// 	return (
+// 	  <Card>
+// 	  	{
+// 	  		work.src ?
+// 	  			<a href={work.src}>
+// 	  			<Image
+// 	  				src={work.thumb}
+// 	  				style={{width: '100%', height: 'auto'}}
+// 	  				loading="lazy"
+// 	  				ui={true} />
+// 	  			</a>
+// 				:
+// 			  	<Image src={work.thumb} wrapped ui={true} />	  		
+// 	  	}
+// 	    	{
+// 	    		work.title && <Card.Content><Card.Header style={{fontWeight:'normal'}}>{work.title}</Card.Header></Card.Content>
+// 	    	}
+// 		    {/*
+// 		    	      <Card.Meta>
+// 	        <span className='date'>{work.date}</span>
+// 	      </Card.Meta>
+// 	      <Card.Description>
+// 	        &quot;{work.input}&quot;
+// 	      </Card.Description>
+// 		    </Card.Content>
+// 				*/}
+// 	    <Card.Content extra>
+// 	    		{
+// 	    			methods.map((m,i) => <Label style={labelsize} key={`l${i}`} tag={i==0 ? true : false}>{m}</Label>)
+// 	    		}
+// 			    <Label style={labelsize}>{work.model}</Label>
+// 			    {work.initial && <Label style={labelsize}>initial</Label>}
+// 			    {work.target  && <Label style={labelsize}>target</Label>}
+// 	    </Card.Content>
+// 	  </Card>
+// 	)
+// } //
+
+class ArtPage extends React.Component {
+  state = {
+    allartworks: [],
+    artworks: [],
+    activebreadcrumb: null,
+    breadcrumbs: []
+  }
+  componentDidMount = () => {
+    // get artworks
+  //   fetch(_ARTWORKS)
+  //   .then(response => {
+  //     if (response.status >= 400) {
+  //       throw new Error("Bad response from server: Could not get projects")
+  //     }
+  //     return response.json()
+  //   })
+  //   .then(artworks => {
+  //   	let dates = []
+  //   	artworks.forEach(series => {
+  //   		series.works.forEach(w => {
+  //   			dates.push(`${w.year}-${w.month}`)
+  //   		})
+  //   	})
+  //   	const breadcrumbs = [...new Set(dates)];
+  //   	breadcrumbs.sort()
+		// 	this.setState({
+		// 		artworks,
+		// 		allartworks: artworks,
+		// 		breadcrumbs //: ['2021-10', '2021-11']
+		// 	})
+		// })
+
+		// inject Twitter timeline script
+		const script = document.createElement("script");
+		script.type = "text/javascript";
+		script.src = 'https://platform.twitter.com/widgets.js';
+		script.charset = 'utf-8';
+		document.body.appendChild(script);
+
+  }
+  // handleFilterClick(d) {
+  // 	if (!d) {
+  // 		// reset to all artworks
+	 //  	this.setState({
+	 //  		artworks: this.state.allartworks,
+	 //  		activebreadcrumb: null,
+	 //  	})
+  // 		return
+  // 	}
+  // 	this.setState({
+  // 		artworks: this.state.allartworks.map(series => {
+  // 			const filteredworks = series.works.filter(w => `${w.year}-${w.month}` === d ? w : false)
+  // 			// filter the whole series if empty
+  // 			if (!filteredworks.length) {
+	 //  			return false	
+  // 			}
+  // 			console.log('works', filteredworks)
+  // 			// replace works in series with the filtered ones
+  // 			const out = {...series}
+  // 			out.works = filteredworks
+  // 			return out
+  // 		}),
+  // 		activebreadcrumb: d,
+  // 	})
+  // }
+  render() {
+  	// const { artworks, activebreadcrumb, breadcrumbs } = this.state
+  	// resizing series with only 1 artwork
+  	// const resizeMapSingle = {
+  	// 	"1": 1,
+  	// 	"2": 1,
+  	// 	"3": 2,
+  	// 	"4": 2,
+  	// 	"5": 2,
+  	// 	"6": 2,
+  	// }
+  	// resizing series with > 1 artworks
+  	// const resizeMapMultiple = {
+  	// 	"1": 1,
+  	// 	"2": 2,
+  	// 	"3": 2,
+  	// 	"4": 2,
+  	// 	"5": 2,
+  	// 	"6": 2,
+  	// }
+    	//<SimpleReactLightbox>
+    return (
+	   	<Layout>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>AI-generated Art {'//'} jonaso.de</title>
+          <link rel="canonical" href="https://www.jonaso.de/art" />
+        </Helmet>
+	      <Container>
+					<Header as='h1' textAlign='center' content="AI-generated Artworks" />
+		        <section style={{textAlign:'center'}}>
+		        	<p>I like to experiment with text-to-image synthesis. The digital artworks below were created with 
+		        		Katherine Crowson's (<a href="https://twitter.com/RiversHaveWings">@RiversHaveWings</a>)
+		        		VQGAN+CLIP, CLIP guided diffusion, etc.
+		        	</p>
+		        	<p>
+		        		I post my AI-generated images on the Twitter account <a href="https://twitter.com/kettlebellz">@kettlebellz</a>.
+		        	</p>
+		        </section>
+
+		        <section id="twitter-timeline-container">
+			        <a class="twitter-timeline" data-lang="en" data-dnt="true" href="https://twitter.com/kettlebellz?ref_src=twsrc%5Etfw">Tweets by kettlebellz</a>
+		        </section>
+
+	          <div className="spacer" style={spacer}></div>
+
+{/*
+						<div id="breadcrumbs">
+							<span style={{...styles.breadcrumb, fontWeight: activebreadcrumb == null ? 'bold' : 'normal'}}
+								onClick={() => this.handleFilterClick(null)}>ALL</span>
+							{
+								breadcrumbs.map((b,i) => (
+									<React.Fragment key={`f${i}`}>
+										<span> / </span>
+										<span style={{
+												...styles.breadcrumb,
+												fontWeight: activebreadcrumb === b ? 'bold' : 'inherit'
+											}}
+											onClick={() => this.handleFilterClick(b)}>{b}</span>
+									</React.Fragment>
+								))
+							}
+						</div>
+
+	          <div className="spacer" style={spacer}></div>
+
+	          <SRLWrapper>
+		        {
+		        	artworks.map((series,s) => series.works ? (
+						      <Container key={`s_${s}`}>
+										<Header as='h2' textAlign='center' content={series.series} />
+
+                    <MediaContextProvider>
+                      <Media at="sm">
+							    	    <Card.Group centered itemsPerRow={1}>
+					    	    			{series.works && series.works.map((w,i) => <Cardwork key={i} info={w} numtiles={series.numtiles} />)}
+											  </Card.Group>
+                      </Media>
+                      <Media at="md">
+							    	    <Card.Group centered itemsPerRow={series.works.length === 1 ? resizeMapSingle[series.numtiles] : resizeMapMultiple[series.numtiles]}>
+					    	    			{series.works && series.works.map((w,i) => <Cardwork key={i} info={w} numtiles={series.numtiles} />)}
+											  </Card.Group>
+                      </Media>
+                      <Media greaterThanOrEqual="lg">
+							    	    <Card.Group centered itemsPerRow={series.numtiles}>
+					    	    			{series.works && series.works.map((w,i) => <Cardwork key={i} info={w} numtiles={series.numtiles} />)}
+											  </Card.Group>
+                      </Media>
+                    </MediaContextProvider>
+
+				            <div className="spacer" style={spacer}></div>
+						      </Container>
+					  	) : null)
+		      	}
+		      	</SRLWrapper>
+
+*/}
+
+	      </Container>
+        </Layout>
+    )
+        // </SimpleReactLightbox>
+  }
+}
+
+
+export default ArtPage
