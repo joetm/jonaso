c6c7ed77be4233456319ed39e62f52b5bd812901
Jonas <jonaso@web.de>
2022-07-17 10:16:29 +0300

    update artwork pages

--- src/components/ArtHeader.js
+++ src/components/ArtHeader.js
@@ -5,29 +5,28 @@ import Layout from "./layout"
 import { Helmet } from "react-helmet"
 import PageSwitcher from './PageSwitcher'
 
-const ArtHeader = ({generator, byline}) => {
+const ArtHeader = ({generator, byline, totalCount}) => {
 	return (
 	   	<Layout>
         <Helmet>
           <meta charSet="utf-8" />
-          <title>{generator} Art {'//'} jonaso.de</title>
+          <title>
+          	{generator === 'AI-generated Artworks' ? generator : `${generator} Art` } {'//'} jonaso.de
+          </title>
           <link rel="canonical" href="https://www.jonaso.de/artworks" />
         </Helmet>
 	      <Container>
 						<Header as='h1' textAlign='center' content='AI-generated Artworks' />
 		        <section style={{textAlign:'center', marginBottom:'2em'}}>
-		        	<p>{byline}</p>
+			        <p>
+		        	{
+		        		byline ?
+		        			byline
+		        				:
+			        		`${totalCount} images`
+		        	}
+							</p>
 		        </section>
-
-			      {/*
-						<div style={{textAlign:'center'}}>
-							Loading: {count}/{totalCount}
-							<div style={{height:'10px',width:'100%',position:'relative',margin:'1rem',border:'1px solid #AAAAAA',overflow:'hidden'}}>
-								<div style={{height:'10px',margin:0,padding:0,position:'relative',width:`${count/totalCount || 0}`,backgroundColor:'#FFFF00'}}>
-								</div>
-							</div>
-						</div>
-						*/}
 
 				<PageSwitcher generator={generator} />
 
