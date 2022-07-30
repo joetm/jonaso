53670dbfa79d034a7857f997cccab737f46183c6
Jonas <jonaso@web.de>
2022-07-30 19:25:11 +0300

    update

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,69 @@
 const rimraf = require('rimraf');
-
 const PUBLIC_FOLDER = `${__dirname}/public`;
-
 exports.onPreBuild = () => {
   // empty /public folder
   rimraf.sync(PUBLIC_FOLDER + '/*');
 };
+
+
+// PAGINATION
+// const path = require("path")
+// // const { createFilePath } = require("gatsby-source-filesystem")
+// exports.createPages = async ({ graphql, actions, reporter }) => {
+//   const { createPage } = actions
+//   const testResult = await graphql(
+//     `{
+//         allFile(
+//           sort: {fields: birthTime, order: DESC}
+//           filter: {
+//             extension: { regex: "/(jpg)|(png)/" }
+//             absolutePath: { regex: "/midjourney/" }
+//           }
+//           limit: 100
+//       ) {
+//         edges {
+//           node {
+//             childImageSharp {
+//               gatsbyImageData(
+//                 width: 400
+//                 placeholder: DOMINANT_COLOR
+//               )
+//             }
+//           }
+//         }
+//       }
+//     }`
+//   )
+//   if (testResult.errors) {
+//     reporter.panicOnBuild(`Error while running GraphQL query.`)
+//     return
+//   }
+//   const posts = testResult.data.allFile.edges
+//   const postsPerPage = 25
+//   const numPages = 4 // Math.ceil(posts.length / postsPerPage)
+//   Array.from({ length: numPages }).forEach((_, i) => {
+//     createPage({
+//       path: `/artworks/midjourney/${i + 1}`,
+//       component: path.resolve("./src/components/page-template.js"),
+//       context: {
+//         limit: postsPerPage,
+//         skip: i * postsPerPage,
+//         numPages,
+//         currentPage: i + 1,
+//       },
+//     })
+//   })
+
+// }
+// // exports.onCreateNode = ({ node, actions, getNode }) => {
+// //   const { createNodeField } = actions
+// //   if (node.internal.type === `MarkdownRemark`) {
+// //     const value = createFilePath({ node, getNode })
+// //     createNodeField({
+// //       name: `slug`,
+// //       node,
+// //       value,
+// //     })
+// //   }
+// // }
+
