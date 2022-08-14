be778900ac271036e571a7bd0b50e95602a0d565
Jonas <jonaso@web.de>
2022-08-14 19:04:02 +0300

    FIX REDRAW BUG

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import { navigate } from "gatsby"
-import ResponsiveMenu from './ResponsiveMenu'
+import { Menu } from 'semantic-ui-react'
+// import ResponsiveMenu from './ResponsiveMenu'
 
 const styles = {
   navSpacer: {
     marginBottom: '2em',
   },
+}
+
+
+const MenuItem = ({activeItem, handleItemClick, item, url = null}) => {
+    const lcitem = item.toLowerCase()
+    return (
+      <Menu.Item
+        name={lcitem}
+        url={url}
+        active={activeItem === lcitem}
+        onClick={handleItemClick}
+      >
+        {item}
+      </Menu.Item>
+    ) //
 }
 
 
@@ -52,7 +68,27 @@ export default class Nav extends React.Component {
     const { activeItem, researchNavOpen, mobileMenuIsOpen } = this.state
     return (
       <header>
-        <ResponsiveMenu active={activeItem} handleItemClick={this.handleItemClick} mobileMenuIsOpen={mobileMenuIsOpen} researchNavOpen={researchNavOpen} />
+        {/*
+          <ResponsiveMenu active={activeItem} handleItemClick={this.handleItemClick} mobileMenuIsOpen={mobileMenuIsOpen} researchNavOpen={researchNavOpen} />
+        */}
+      <div id="desktopmenu">
+        <Menu fluid pointing stackable primary="true" widths="7">
+          <MenuItem active={activeItem === 'home'} item='Home' handleItemClick={this.handleItemClick} />
+          <MenuItem active={activeItem === 'publications'} item='Publications' handleItemClick={this.handleItemClick} />
+          <MenuItem active={activeItem.startsWith('artworks')} item='Artworks' link={false} header={true} handleItemClick={this.handleItemClick} />
+          <MenuItem active={activeItem === 'projects'} item='Projects' handleItemClick={this.handleItemClick} />
+          <MenuItem active={activeItem === 'cv'} item='CV' handleItemClick={this.handleItemClick} />
+          <MenuItem active={activeItem.startsWith('research')} item='Research' link={false} header={true} handleItemClick={this.handleItemClick} />
+        </Menu>
+        {
+          researchNavOpen &&
+            <Menu pointing stackable secondary size="small" widths="7">
+              <MenuItem active={activeItem === 'researchinterests'} item='Interests' url='/research/interests' handleItemClick={this.handleItemClick} />
+              <MenuItem active={activeItem === 'researchreading'} item='Reading' url='/research/reading' handleItemClick={this.handleItemClick} />
+              <MenuItem active={activeItem === 'researchinfluences'} item='Influences' url='/research/influences' handleItemClick={this.handleItemClick} />
+            </Menu>
+        }
+        </div>
         <div style={styles.navSpacer}></div>
       </header>
     )
