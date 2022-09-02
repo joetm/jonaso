62b4ef9b7febb665af0add55e1c6d5a039117fa1
Jonas <jonaso@web.de>
2022-09-02 08:47:11 +0300

    fix publisher field in publications.json

---
+++ fix-publications-json.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import json
+
+# fix the publisher field:
+# split into howpublished and publisher
+
+f = open('./src/bibliography/publications.json')
+# f = open('./public/static/publications.json')
+data = json.load(f)
+f.close()
+
+for obj in data:
+	try:
+		howpublished, publisher = obj['publisher'].split(";")
+		howpublished = howpublished.strip()
+		publisher = publisher.strip()
+	except:
+		continue
+	obj['howpublished'] = howpublished
+	obj['publisher'] = publisher
+
+with open('./public/static/publications-TEST.json', 'w') as f:
+    json.dump(data, f, indent=2, sort_keys=True)
