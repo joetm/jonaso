21a456b9c9ad462087f4f94bd6ce4301a89d2e29
Jonas <jonaso@web.de>
2022-12-30 20:22:17 +0200

    fix active menu item; reorder nav

--- src/pages/research.js
+++ src/pages/research.js
@@ -1,6 +1,7 @@
 import * as React from "react"
-import Interests from './research/interests.js'
+// import Interests from './research/interests.js'
+import Publications from './publications.js'
 
 export default function Component () {
-  return (<Interests />)
+  return (<Publications />)
 }
