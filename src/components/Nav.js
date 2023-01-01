92c750353c3a9290cdc04d018aa88945de1d8c02
Jonas <jonaso@web.de>
2023-01-01 10:50:57 +0200

    update

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -66,16 +66,16 @@ export default class Nav extends React.Component {
           <Menu fluid pointing stackable primary="true" widths="7">
             <MenuItem key="home" active={activeItem === 'home'} item='Home' handleItemClick={this.handleItemClick} />
             <MenuItem key="artworks" active={activeItem.startsWith('artworks')} item='Artworks' link={false} header={true} handleItemClick={this.handleItemClick} />
-            <MenuItem key="cv" active={activeItem === 'cv'} item='CV' handleItemClick={this.handleItemClick} />
+            <MenuItem key="publications" active={activeItem === 'publications'} item='Publications' handleItemClick={this.handleItemClick} />
             <MenuItem key="research" active={activeItem.startsWith('research') || activeItem === 'publications' || activeItem === 'projects'} item='Research' link={false} header={true} handleItemClick={this.handleItemClick} />
+            <MenuItem key="cv" active={activeItem === 'cv'} item='CV' handleItemClick={this.handleItemClick} />
           </Menu>
           {
             researchNavOpen &&
               <Menu pointing stackable secondary size="small" widths="7">
-                <MenuItem key="publications" active={activeItem === 'research' || activeItem === 'publications'} item='Publications' handleItemClick={this.handleItemClick} />
-                <MenuItem key="projects" active={activeItem === 'projects'} item='Projects' handleItemClick={this.handleItemClick} />
-                <MenuItem key="researchinterests" active={activeItem === 'researchinterests'} item='Interests' url='/research/interests' handleItemClick={this.handleItemClick} />
-                <MenuItem key="researchreading" active={activeItem === 'researchreading'} item='Reading' url='/research/reading' handleItemClick={this.handleItemClick} />
+                <MenuItem key="researchinterests"  active={activeItem === 'research' || activeItem === 'researchinterests'}  item='Interests' url='/research/interests' handleItemClick={this.handleItemClick} />
+                <MenuItem key="researchprojects"   active={activeItem === 'researchprojects'}   item='Projects' url='/research/projects' handleItemClick={this.handleItemClick} />
+                <MenuItem key="researchreading"    active={activeItem === 'researchreading'}    item='Reading' url='/research/reading' handleItemClick={this.handleItemClick} />
                 <MenuItem key="researchinfluences" active={activeItem === 'researchinfluences'} item='Influences' url='/research/influences' handleItemClick={this.handleItemClick} />
               </Menu>
           }
