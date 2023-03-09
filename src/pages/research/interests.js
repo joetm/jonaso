96acab762456f30447dd0536436dd8cc59ee1f1a
Jonas <jonaso@web.de>
2023-03-09 14:50:45 +0200

    no jumping on interests page

--- src/pages/research/interests.js
+++ src/pages/research/interests.js
@@ -4,7 +4,7 @@ import Layout from "../../components/layout"
 import GraphSwitcher from "../../components/GraphSwitcher"
 import { Seo } from "../../components/Seo"
 import { spacer } from "../../common"
-
+import { Button } from 'semantic-ui-react'
 
 const _KEYWORDS = 'https://raw.githubusercontent.com/joetm/jonaso/master/reading_list/keywords.json'
 
@@ -20,6 +20,14 @@ export function Head() {
 class Interests extends React.Component { //
   state = {
     keywords: [],
+    isZoomed: false,
+    breadcrumb: null,
+  }
+  zoom = (val, bc = null) => {
+    this.setState({
+      isZoomed: val,
+      breadcrumb: bc,
+    })
   }
   componentDidMount = () => {
     fetch(_KEYWORDS)
@@ -32,17 +40,24 @@ class Interests extends React.Component { //
     .then(keywords => this.setState({ keywords }))
   }
   render() {
-    const { keywords } = this.state
+    const { keywords, isZoomed, breadcrumb } = this.state
     const filtered_keywords = keywords.filter(kw => kw.num > 19)
     return (
       <Layout>
         <div className="ui container">
-          <h2>
-            Research Interests
-            {' '}
-            <GraphSwitcher active="interests" />
-          </h2>
-          <Keywords keywords={filtered_keywords} />
+          <div className="ui segment" style={{clear:'both', border:0, boxShadow: '0px 0px 0px #FFFFFF'}}>
+              <h2 style={{float:'left', display:'inline-block', marginRight: '1rem'}}>Research Interests</h2>
+                {' '}
+                <GraphSwitcher active="interests" />
+              {
+                isZoomed &&
+                  <div style={{float:'right', fontSize: 'initial', marginRight:'1em'}}>
+                    <span style={{marginRight: '1em'}}>{breadcrumb}</span>
+                    <Button circular onClick={() => this.zoom(false)} icon='left arrow' />
+                  </div>
+              }
+          </div>
+          <Keywords keywords={filtered_keywords} isZoomed={isZoomed} zoom={this.zoom} />
           <div style={spacer}></div>
         </div>
       </Layout>
