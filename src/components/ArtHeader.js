18c23698c77a526a9054d28fcea42e5eb96b7589
Jonas <jonaso@web.de>
2023-03-12 18:46:39 +0200

    improvements to art page switcher

--- src/components/ArtHeader.js
+++ src/components/ArtHeader.js
@@ -1,10 +1,15 @@
 import React from "react"
 import Layout from "./layout"
 import { Title, Link, Meta } from "react-head"
-import ArtPageSwitcher from './ArtPageSwitcher'
+import { navigate } from 'gatsby'
 
 
 const ArtHeader = ({generator, byline, totalCount}) => {
+  const handleMenuClick = (e) => {
+    e.preventDefault()
+    const folder = e.target.getAttribute('folder')
+    navigate(`/artworks/${folder}/`)
+  }
   return (
       <Layout>
           <Meta charSet="utf-8" />
@@ -12,6 +17,7 @@ const ArtHeader = ({generator, byline, totalCount}) => {
           <Link rel="canonical" href="https://www.jonaso.de/artworks/" />
           <div className="ui container">
             <h1>{generator}</h1>
+
             <section style={{textAlign:'center', marginBottom:'2em'}}>
               <p>
               {
@@ -23,7 +29,54 @@ const ArtHeader = ({generator, byline, totalCount}) => {
               }
               </p>
             </section>
-            <ArtPageSwitcher generator={generator} />
+
+            <section style={{textAlign:'center', marginBottom:'2em'}}>
+              <div className="ui fluid pointing secondary six item menu">
+                <a
+                  folder="midjourney"
+                  className={"item" + (generator === 'Midjourney' ? ' active' : '')}
+                  onClick={handleMenuClick}
+                >
+                  Midjourney
+                </a>
+                <a
+                  folder="stablediffusion"
+                  className={"item" + (generator === 'Stable Diffusion' ? ' active' : '')}
+                  onClick={handleMenuClick}
+                >
+                  Stable Diffusion
+                </a>
+                <a
+                  folder="dalle"
+                  className={"item" + (generator === 'DALL-E' ? ' active' : '')}
+                  onClick={handleMenuClick}
+                >
+                  DALL-E 2
+                </a>
+                <a
+                  folder="latent-diffusion"
+                  className={"item" + (generator === 'Latent Diffusion' ? ' active' : '')}
+                  onClick={handleMenuClick}
+                >
+                  Latent Diffusion
+                </a>
+                <a
+                  folder="vqganclip"
+                  className={"item" + (generator === 'VQGAN-CLIP' ? ' active' : '')}
+                  onClick={handleMenuClick}
+                >
+                  VQGAN-CLIP
+                </a>
+                <a
+                  folder="misc"
+                  className={"item" + (generator === 'Misc. Text-To-Image Systems' ? ' active' : '')}
+                  onClick={handleMenuClick}
+                >
+                  Misc.
+                </a>
+                  </div>
+            </section>
+
           </div>
         </Layout>
   )
