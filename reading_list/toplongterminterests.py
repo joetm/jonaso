e94042c77923e20efa0d859805cdb204c1292d25
Jonas <jonaso@web.de>
2023-03-30 15:40:31 +0300

    add zeros in data

--- reading_list/toplongterminterests.py
+++ reading_list/toplongterminterests.py
@@ -111,6 +111,13 @@ for ts in timeline.keys():
   obj = timeline[ts]
   obj['t'] = ts
   outdata.append(obj)
+# fill missing keys
+keys = legend.keys()
+for i in range(len(outdata)):
+  o = outdata[i]
+  for k in keys:
+    if not k in o:
+      outdata[i][k] = 0
 
 # print(timeline)
 output = {
