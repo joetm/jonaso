56865ac3e08de4bafef27687d385f1d0829ff8cc
Jonas <jonaso@web.de>
2023-06-17 11:51:24 +0300

    update jufo

--- src/pages/jufo.jsx
+++ src/pages/jufo.jsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React from "react"
+import React, { useState } from "react"
 import Layout from "../components/layout"
 import { Seo } from "../components/Seo"
 import { useQuery } from '@tanstack/react-query'
@@ -18,27 +18,60 @@ export function Head() {
 }
 
 
+
 export default function JufoPage() {
+  const [ pubs, storePubs ] = useState([])
   const { isLoading, error, data } = useQuery({
     queryKey: ['references-detail.json'],
     queryFn: () => fetch(_PUB_URL)
               .then((res) => res.json())
-              .then((pubs) => pubs.map(x => x.jufo ? x.jufo : 0).reduce(( previousValue, currentValue ) => parseInt(previousValue, 10) + parseInt(currentValue, 10), 0))
+              .then((pubs) => {
+                storePubs(pubs)
+                return pubs.map(x => x.jufo ? x.jufo : 0).reduce(( previousValue, currentValue ) => parseInt(previousValue, 10) + parseInt(currentValue, 10), 0)
+              })
   })
+
+  function parseTitle(title) {
+    // title = title.replace('\\\'{c}', 'ć')
+    title = title.replace('\\"a', 'ä')
+    title = title.replace('\\"u', 'ü')
+    title = title.replace('\\\"u', 'ü')
+    title = title.replace('\\"o', 'ö')
+    title = title.replace('\{', '')
+    title = title.replace('\}', '')
+    title = title.replace('{', '')
+    title = title.replace('}', '')
+    title = title.replace('{', '')
+    title = title.replace('}', '')
+    return title
+  }
+
   return (
     <Layout>
       <div className="ui container">
         <h1>Jufo Points</h1>
         <section style={{textAlign:'center'}}>
           <p style={{fontSize:'22pt'}}>
-            {
-              error && 'An error has occurred: ' + error.message
-            }
+            { error && 'An error has occurred: ' + error.message }
             {
               isLoading ? '...loading...' : <span>&asymp; {data}</span>
             }
           </p>
         </section>
+        {
+          pubs.length &&
+            <section style={{textAlign:'center'}}>
+              <ul style={{textAlign: 'left'}}>
+                {
+                  pubs.map(p => <li key={p.title}>
+                    {p.jufo}:{' '}
+                    {parseTitle(p.title)}
+                    {p.booktitle && ' (' + p.booktitle + ')'}
+                    </li>)
+                }
+              </ul>
+            </section>
+        }
       </div>
     </Layout>
   )
