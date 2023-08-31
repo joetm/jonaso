ad3c8c8a906fb32e0b55edd00c652747ed90af6e
Jonas <jonaso@web.de>
2023-08-31 21:50:20 +0300

    update author list with recency factor

--- reading_list/create_allauthors.py
+++ reading_list/create_allauthors.py
@@ -2,9 +2,22 @@
 # -*- coding: utf-8 -*-
 
 import json
+from collections import defaultdict
+import datetime
+
+# Define the current year
+today = datetime.date.today()
+current_year = today.year
+
 
 with open('influencer.json') as f:
 	influencers = json.load(f)
+
+# decay factor (between 0 and 1)
+# The Decay Factor is a constant between 0 and 1. A decay factor close to 1 means the weight drops off very slowly for older papers, whereas a decay factor close to 0 means it drops off very quickly.
+decay_factor = 0.5
+
+recency_dict = defaultdict(float)
 
 allauthors = []
 
@@ -12,6 +25,16 @@ for prio in influencers.keys():
 	if int(prio) > 0:
 		for author in influencers[prio]:
 			author['priority'] = int(prio)
+
+			years = [ datetime.datetime.fromtimestamp(ts).year for ts in author['modified'] ]
+
+			del author['modified']
+
+			author['recency'] = 0
+
+			for year in years:
+				author['recency'] += author['priority'] * (decay_factor ** (current_year - year))	        
+
 			allauthors.append(author)
 
 with open('allauthors.json', 'w') as out:
