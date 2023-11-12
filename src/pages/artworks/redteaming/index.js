da4fece41f92c5ecb1680c2500f23b983d9a2f89
Jonas <jonaso@web.de>
2023-11-12 19:50:21 +0200

    update

---
+++ src/pages/artworks/redteaming/index.js
@@ -0,0 +1,33 @@
+"use client"
+
+import '../../../react-masonry.css'
+
+import React, { useState, useEffect } from "react"
+import ArtHeader from '../../../components/ArtHeader'
+import Layout from "../../../components/layout"
+import MasonryGallery from '../../../components/MasonryGallery'
+
+
+// import images from "../../../../public/artworks/json/webp-dalle-2.json"
+const cachebuster = Math.round(Date.now() / 10000)
+const _URL = process.env.NODE_ENV === "development" ? `/artworks/json/webp-redteaming.json?${cachebuster}` : `https://raw.githubusercontent.com/joetm/jonaso/master/public/artworks/json/webp-redteaming.json?${cachebuster}`
+
+
+export default function ArtPage() {
+  const [images, setImages] = useState([])
+  useEffect(() => {
+    const dataFetch = async () => {
+      const data = await ( await fetch(_URL) ).json()
+      setImages(data)
+    }
+    dataFetch()
+  }, [])
+  return (
+    <>
+      <Layout style={{paddingBottom: 0}}>
+        <ArtHeader generator='Redteaming' totalCount={images.length} />
+      </Layout>
+      <MasonryGallery images={images} />
+    </>
+  )
+}
