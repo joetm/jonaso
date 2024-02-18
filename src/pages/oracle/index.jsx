cc2cecfbbe02ae3d4262a836cd890c3d2f89164b
Jonas <jonaso@web.de>
2024-02-18 12:46:46 +0200

    update

--- src/pages/oracle/index.jsx
+++ src/pages/oracle/index.jsx
@@ -5,6 +5,8 @@ import 'semantic-ui-css/components/feed.min.css'
 import 'semantic-ui-css/components/message.min.css'
 import 'semantic-ui-css/components/loader.min.css'
 import 'semantic-ui-css/components/list.min.css'
+import 'semantic-ui-css/components/checkbox.min.css'
+
 
 import React, { useState, useEffect, useRef } from "react"
 import Layout from "../../components/layout"
@@ -15,12 +17,25 @@ import stats from "../../../oracle/stats.json"
 
 export const isProd = process.env.NODE_ENV !== "development"
 
+const HISTORY_LENGTH = 3
+
 if (isProd) {
   // TODO
   const URL = 'http://0.0.0.0:8080/query'
 } else {
   const URL = 'http://0.0.0.0:8080/query'
 }
+
+const preFabExamples = [
+  "What are the difficulties of moderating twitch communities?",
+  "What are the challenges of generative deep learning?",
+  "List some prompt engineering techniques.",
+  "What are systems, libraries, tools for data validation?",
+  "What are the issues with mobile phones?",
+  "What are topics in Collective Intelligence?",
+  "What is Methodolatry?",
+  "Who was living in airports?",
+]
 
 const styles = {
   exampleContainer: {
@@ -59,14 +74,18 @@ export function Head() {
 }
 
 
+
 export default function Chat() {
-  const [chathistory, setChathistory] = useState([
+  const [chat, updateChat] = useState([
     // { 'role': 'user', 'msg': 'test msg'},
     // { 'role': 'oracle', 'msg': 'ok'},
   ])
+  const [chatHistory, updateChatHistory] = useState([])
   const [isLoading, setIsLoading] = useState(false)
   const [firstUse, setFirstUse] = useState(true)
+  const [useHyperlinks, setUseHyperlinks] = useState(false)
   const chatRef = useRef()
+  const inputRef = useRef()
 
   useEffect(() => {
     if (chatRef.current) {
@@ -76,13 +95,24 @@ export default function Chat() {
       // Execute scroll after the next repaint to ensure DOM has updated
       requestAnimationFrame(scroll)
     }
-  }, [chathistory])
+  }, [chat])
+
+  // focus the input when it is no longer deactivated
+  useEffect(() => {
+    // Only try to focus when isLoading is false and the input is presumably active
+    if (!isLoading) {
+      inputRef.current.focus()
+    }
+  }, [isLoading])
 
   // body: JSON.stringify({ query: "What are the difficulties of moderating twitch communities?" }),
 
   // Use functional update to ensure we're working with the most current state
   const handleServerResponse = (data) => {
-    setChathistory(chathistory => [...chathistory, data])
+    updateChat(chat => [...chat, data])
+    if (data.role == "user") {
+      updateChatHistory(chatHistory => [...chatHistory, data].slice(-1 * HISTORY_LENGTH))
+    }
   }
 
   const sendIt = (new_msg) => {
@@ -91,7 +121,7 @@ export default function Chat() {
     fetch('http://0.0.0.0:8080/query', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({query: new_msg}),
+      body: JSON.stringify({query: new_msg, history: chatHistory}),
     })
     .then(response => response.json())
     .then(data => {
@@ -105,41 +135,41 @@ export default function Chat() {
     })
   }
 
-  function handleListItemClick(txt) {
-    txt = "What are topics in " + txt
+  const handleListItemClick = (txt) => {
+    txt = "Tell me more about " + txt
     sendIt(txt)
   }
 
-const TextWithLinks = ({ text }) => {
-  // Find the first comma
-  const firstCommaIndex = text.indexOf(',');
-  if (firstCommaIndex === -1) {
-    // If there's no comma, just return the text as is
-    return <>{text}</>
+  const TextWithLinks = ({ text }) => {
+    // Find the first comma
+    const firstCommaIndex = text.indexOf(',')
+    if (firstCommaIndex === -1) {
+      // If there's no comma, just return the text as is
+      return <>{text}</>
+    }
+    // Extract the text before the first comma (excluding the immediate word that we'll linkify)
+    const introText = text.substring(0, text.lastIndexOf(' ', firstCommaIndex))
+    // Extract the word immediately before the first comma and the rest of the list items
+    const listStartsAt = text.lastIndexOf(' ', firstCommaIndex) + 1
+    const listText = text.substring(listStartsAt)
+    // Split the remaining text into items based on commas and 'and'
+    const items = listText.split(/,| and | include /).filter(Boolean)
+    // Function to render a link for each item
+    const renderItemAsLink = (item, index) => (
+      <React.Fragment key={index}>
+        {/* Add a comma before items except the first */}
+        {index > 0 && ', '}
+        <a href="#" onClick={() => handleListItemClick(item.trim())}>
+          {item.trim()}
+        </a>
+      </React.Fragment>
+    )
+    return (
+      <>
+        {introText} {items.map(renderItemAsLink)}
+      </>
+    )
   }
-  // Extract the text before the first comma (excluding the immediate word that we'll linkify)
-  const introText = text.substring(0, text.lastIndexOf(' ', firstCommaIndex));
-  // Extract the word immediately before the first comma and the rest of the list items
-  const listStartsAt = text.lastIndexOf(' ', firstCommaIndex) + 1;
-  const listText = text.substring(listStartsAt);
-  // Split the remaining text into items based on commas and 'and'
-  const items = listText.split(/,| and /).filter(Boolean);
-  // Function to render a link for each item
-  const renderItemAsLink = (item, index) => (
-    <React.Fragment key={index}>
-      {/* Add a comma before items except the first */}
-      {index > 0 && ', '}
-      <a href="#" onClick={() => handleListItemClick(item.trim())}>
-        {item.trim()}
-      </a>
-    </React.Fragment>
-  )
-  return (
-    <>
-      {introText} {items.map(renderItemAsLink)}
-    </>
-  )
-}
 
   const handleKeyDown = (event) => {
     if (event.key === 'Enter') {
@@ -152,16 +182,32 @@ const TextWithLinks = ({ text }) => {
     }
   }
 
-  const handleExampleClick = (e) => {
+  const handleExampleClick = (msg) => {
     setFirstUse(false)
-    sendIt(e.target.innerHTML)
+    sendIt(msg)
   }
+
+  const handleHyperlinkOption = (e) => setUseHyperlinks(e.target.checked)
 
   return (
     <Layout style={{marginBottom:0, paddingBottom:0}}>
       <div className="ui message">
         You are chatting with {stats?.total} documents that I have <Link to="/research/reading/">read</Link> and found <Link to="/research/influences/">interesting</Link>.
-        <div style={{float: 'right'}}><a href="/oracle">restart</a></div>
+        <div style={{
+          float: 'right',
+        }}>
+          <div
+            className="ui checkbox"
+            style={{
+              display: 'inline-block',
+              borderRight: '2px solid gray',
+              paddingRight: '1rem',
+              fontSize: 'inherit',
+            }}
+          >
+            <input onChange={handleHyperlinkOption} type="checkbox" name="hyperlinks" /><label>hyperlinks</label>
+          </div>
+          <a style={{paddingLeft: '1rem'}} href="/oracle">restart</a></div>
       </div>
       <div className="ui container" style={{
           backgroundColor: '#DDDDDD',
@@ -175,16 +221,22 @@ const TextWithLinks = ({ text }) => {
         ref={chatRef}
       >
 
-        {chathistory.map((msgObj, index) => {
+        {chat.map((msgObj, index) => {
           const isUserMsg = msgObj['role'] == 'user'
           return (
-            <div key={index} style={{marginBottom: '25px', clear: 'both'}}>
+            <div key={index} style={{marginBottom: '5px'}}>
               {/*
                 <div className="header" style={{textAlign: isUserMsg ? 'right' : 'left'}}>{isUserMsg ? 'you' : 'Oracle'}</div>
               */}
-              <div className="ui message" style={{float: isUserMsg ? 'right' : 'left', maxWidth: '80%'}}>
-                <p><TextWithLinks text={msgObj.msg} isUser={isUserMsg} /></p>
+              <div className="ui message" style={{float: isUserMsg ? 'right' : 'left', maxWidth: '80%', backgroundColor: isUserMsg ? '#888888' : 'white', color: isUserMsg ? 'white' : 'black' }}>
+                {
+                  useHyperlinks ?
+                    <TextWithLinks text={msgObj.msg} isUser={isUserMsg} />
+                  :
+                    msgObj.msg
+                }
               </div>
+              <div style={{clear: 'both'}}></div>
             </div>
           )}
         )}
@@ -199,36 +251,15 @@ const TextWithLinks = ({ text }) => {
         {
           firstUse &&
             <div className="examplecontainer" style={styles.exampleContainer}>
-              <div onClick={handleExampleClick} className="item" style={styles.exampleContainer.example}>
-                <div className="content">
-                  What are the difficulties of moderating twitch communities?
-                </div>
-              </div>
-              <div onClick={handleExampleClick} className="item" style={styles.exampleContainer.example}>
-                <div className="content">
-                  What are the challenges of generative deep learning?
-                </div>
-              </div>
-              <div onClick={handleExampleClick} className="item" style={styles.exampleContainer.example}>
-                <div className="content">
-                  List some prompt techniques.
-                </div>
-              </div>
-              <div onClick={handleExampleClick} className="item" style={styles.exampleContainer.example}>
-                <div className="content">
-                  What are systems, libraries, tools for data validation?
-                </div>
-              </div>
-              <div onClick={handleExampleClick} className="item" style={styles.exampleContainer.example}>
-                <div className="content">
-                  What are the issues with mobile phones?
-                </div>
-              </div>
-              <div onClick={handleExampleClick} className="item" style={styles.exampleContainer.example}>
-                <div className="content">
-                  What are topics in Collective Intelligence?
-                </div>
-              </div>
+              {
+                preFabExamples.map(ex => (
+                  <div key={ex} onClick={() => handleExampleClick(ex)} className="item" style={styles.exampleContainer.example}>
+                    <div className="content">
+                      {ex}
+                    </div>
+                  </div>
+                ))
+              }
             </div>
         }
 
@@ -239,6 +270,7 @@ const TextWithLinks = ({ text }) => {
         marginTop: '20px',
       }}>
         <input
+          ref={inputRef}
           autoFocus
           disabled={isLoading}
           type="text"
