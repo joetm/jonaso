d052702f7aa6ec36c37a90781310ca953f966d4c
Jonas Oppenlaender <oppenlaender@gmail.com>
2026-01-16 12:58:25 +0200

    update index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -184,10 +184,10 @@ export default function Home() {
                 </h3>
                 <ul>
                   <li style={{paddingBottom:'.5rem'}}>
-                    My late-breaking-work <i>&quot;StatCounter: A Longitudinal Study of a Portable Scholarly Metric Display&quot;</i> has been accepted at ACM PerDis 2026.
+                    Our paper <a href="https://default-images.github.io/" target="_blank"><i>&quot;An Exploration of Default Images in Text-to-Image Generation&quot;</i></a> has been accepted at ACM CHI 2026.
                   </li>
                   <li style={{paddingBottom:'.5rem'}}>
-                    Our paper <a href="https://default-images.github.io/" target="_blank"><i>&quot;An Exploration of Default Images in Text-to-Image Generation&quot;</i></a> has been accepted at ACM CHI 2026.
+                    My late-breaking-work <i>&quot;StatCounter: A Longitudinal Study of a Portable Scholarly Metric Display&quot;</i> has been accepted at ACM PerDis 2026.
                   </li>
                   <li style={{paddingBottom:'.5rem'}}>
                     Our <a href="https://meta-hci.github.io/" target="_blank">Meta-HCI Meet-up</a> on Practising Reflection in HCI Research was accepted at ACM CHI '26.
